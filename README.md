e26f3baf858de08257cabddf351e60d45350c6ac
willmil11 <willmil1110@gmail.com>
2022-10-22 14:59:18 +0200

    Finalised the Webserver

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Put your html files in the Files folder
 To customise 404 error message: Replace the default page in the 404 folder with a custom 404 page
 [The custom 404 page must be named '404.html' to work]
 
-To run the server execute in a cmd 'node webserver.js' while being in the Webserver directory
+To run the server execute in a cmd 'node webserver.js' while being in the Webserver directory, you need to run this as root [Don't worry, I patched every security problems :)]
 
 If you have trouble using the Webserver:
 -Email: willmil1110@gmail.com
--- Webserver/Files/JS/main.js
+++ Webserver/Files/JS/main.js
@@ -1,1 +1,26 @@
-document.body.style.backgroundColor = "black";
+console.info("[JS] Js loaded !");
+
+document.body.style.backgroundColor = "rgb(0, 255, 240)"
+
+let button = document.getElementById("main_pagebtn");
+let cropbox = document.getElementById("cropbox");
+
+cropbox.style.backgroundColor = "white";
+cropbox.style.borderRadius = "15%"
+cropbox.style.width = "300px";
+
+button.style.backgroundColor = "black";
+button.style.color = "white";
+button.style.border = "none"
+
+button.onmouseover = function(){
+button.style.background.color = "rgb(255, 85, 85)"
+}
+
+button.onmouseleave = function(){
+button.style.background.color = "black";
+}
+
+button.onclick = function(){
+window.location.href = "/";
+}
